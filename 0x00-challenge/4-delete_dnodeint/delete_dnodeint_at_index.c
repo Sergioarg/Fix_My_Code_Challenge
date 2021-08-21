89688cc729f318de00fdb4ea26cbb924a984ca06
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-21 11:09:34 -0500

    :rewind: Back to base code

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int p;
 
-	if (*head == NULL || saved_head == NULL || tmp == NULL)
+	if (*head == NULL)
 	{
 		return (-1);
 	}
