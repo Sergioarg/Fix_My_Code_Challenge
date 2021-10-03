79e3e7b12f5f6cc9206abef15c8136f43b2b5b9a
Sergioarg <sergio_ramos53@yahoo.com>
2021-10-03 12:57:01 -0500

    :wrench: Fixing python code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
+""" Script that verify area and perimeter of square """
 
 
-class square():
-
-    width = 0
-    height = 0
+class Square():
+    """ New Class Square """
 
     def __init__(self, *args, **kwargs):
+        """ Values of constructor """
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ Perimetrer of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation """
         return "{}/{}".format(self.width, self.height)
 
 
@@ -26,4 +30,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
